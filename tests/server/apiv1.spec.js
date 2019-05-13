1db822c1fb258f40d9b22b54459f35244de50e40
Jianzhuo Shen <2650826828@qq.com>
2019-05-13 02:48:17 +0000

    test 1-5

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -4,7 +4,7 @@
   'use strict';
 
 	var requireHelper = require('./requireHelper');
-  var apiv1 = requireHelper.require('tests/coverage/instrumented/routes/apiv1');
+  var apiv1 = requireHelper.require('/routes/apiv1');
   var assert = require('chai').assert;
   var sinon = require('sinon');
 
@@ -41,7 +41,76 @@
       assert(resMock.status.lastCall.calledWith(400), 'Unexpected status code:' + resMock.status.lastCall.args);
     });
 
-    
+    it('with valid zip code and error from request call', function() {
+      reqMock = {
+        query: {
+          zip: 'Wellington'
+        }
+      };
+
+      var request = function( obj, callback ){
+        callback("error", null, null);
+      };
+
+      apiv1.__set__("request", request);
+
+      apiv1.getWeather2(reqMock, resMock);
+
+      assert(resMock.status.lastCall.calledWith(400), 'Unexpected response:' + resMock.status.lastCall.args);
+      assert(resMock.send.lastCall.calledWith('Failed to get the data'), 'Unexpected response:' + resMock.send.lastCall.args);
+    });
+
+    it('with incomplete zip code', function() {
+      reqMock = {
+        query: {
+          zip: 'Well'
+        }
+      };
+
+      var request = function( obj, callback ){
+        callback(null, null, {});
+      };
+
+      apiv1.__set__("request", request);
+
+      apiv1.getWeather2(reqMock, resMock);
+
+      assert(resMock.status.lastCall.calledWith(400), 'Unexpected response:' + resMock.status.lastCall.args);
+      assert(resMock.send.lastCall.args[0].msg === 'Failed', 'Unexpected response:' + resMock.send.lastCall.args);
+    });
+
+    it('with valid zip code', function() {
+      reqMock = {
+        query: {
+          zip: 'Wellington'
+        }
+      };
+
+      var body = {
+        cod: 200,
+        name: 'Wellington',
+        weather: [
+          {
+            main: 'cold'
+          }
+        ],
+        main: {
+          temp: 78
+        }
+      };
+
+      var request = function( obj, callback ){
+        callback(null, null, body);
+      };
+
+      apiv1.__set__("request", request);
+
+      apiv1.getWeather2(reqMock, resMock);
+
+      assert(resMock.status.lastCall.calledWith(200), 'Unexpected response:' + resMock.status.lastCall.args);
+      assert(resMock.send.lastCall.args[0].city === 'Wellington', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
+      assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 78 C', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
+    });
   });
 /*
   describe('Get Weather 2', function() {
